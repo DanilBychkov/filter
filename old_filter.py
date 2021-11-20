bd289738c473ee1d1ffc86f739e8ed763ef38965
DanilBychkov <81550189+DanilBychkov@users.noreply.github.com>
2021-11-20 09:31:47 +0500

    Add files via upload

---
+++ old_filter.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import numpy as np
+img = Image.open("img2.jpg")
+arr = np.array(img)
+a = len(arr)
+a1 = len(arr[1])
+i = 0
+while i < a - 11:
+    j = 0
+    while j < a1 - 11:
+        s = 0
+        for n in range(i, i + 10):
+            for n1 in range(j, j + 10):
+                s+= arr[n][n1][0]
+                s+= arr[n][n1][1]
+                s+= arr[n][n1][2]
+        s = int(s // 100)
+        for n in range(i, i + 10):
+            for n1 in range(j, j + 10):
+                arr[n][n1][0] = int(s // 50) * 50
+                arr[n][n1][1] = int(s // 50) * 50
+                arr[n][n1][2] = int(s // 50) * 50
+        j = j + 10
+    i = i + 10
+res = Image.fromarray(arr)
+res.save('res.jpg')
